01b59d979357f14a7b2ebb7012a4f9aa3a0535aa
jairo <jairocoppis@outlook.com>
2022-10-11 21:03:05 -0300

    Agregando componentes

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Nosotros</li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    );
+}
+
+export default Header
