b66c333738a4d344b59e32723fa2931c9b3251fb
jairo <jairocoppis@outlook.com>
2022-10-13 18:08:30 -0300

    Agregando component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
     <Header/>
+    <Main/>
     <Footer/>
     </>
   );
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <article>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae voluptas tenetur eum! Qui consequuntur nisi laborum, tenetur amet itaque autem eius, totam assumenda veniam, neque ullam unde a natus dolores?
+        </article>
+    )
+}
+
+export default Main
