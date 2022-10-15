d91077c35602e1edc4a7bfd5aa92338b109ec1dd
jairo <jairocoppis@outlook.com>
2022-10-15 18:33:39 -0300

    agregando logo

---
+++ src/components/CartWidget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>
+        <img className="logo" src="./img/starbuckslogo.png" alt="" />
+    </div>
+  )
+}
+
+export default CartWidget
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Button } from './Button'
+import CartWidget from './CartWidget'
 
 export const Navbar = () => {
   return ( //Agrgando CSS con className
             
             <nav className="navbarstyle"> 
+              <div>
+                <CartWidget/>
+              </div>
                 <ul>
                     <li>Inicio</li>
-                    <li>Nosotros</li>
+                    <li>Catalogo</li>
                 </ul>
                   <div>
                       <span className="material-symbols-outlined"> 
